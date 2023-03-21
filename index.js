07d984ef230f45af5b63fd0aab9b48b337614234
lewisky <lewisky28mawira@gmail.com>
2023-03-21 08:46:30 +0300

    complete assignment

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
 
 // Write your code here
+const num1 = 31;
+const num2 = 2;
+const multiply = num1 * num2;
+const random = Math.ceil(Math.random() * 100);
+const num3 = 14;
+const num4 = 5;
+const mod = num3 % num4;
+const numbers = [3, 5, 1, 8, 20, 4];
+const max = Math.max(...numbers);
